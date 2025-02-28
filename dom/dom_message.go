434f9494c775d4737f5a85361e39c70e782fe119
Lazataknes <alexanderglavk@gmail.com>
2025-02-28 21:30:25 +0300

    Implementation of ShowConfirm and ShowPrompt

--- dom/dom_message.go
+++ dom/dom_message.go
@@ -6,3 +6,13 @@ import "syscall/js"
 func ShowAlert(msg string) {
 	js.Global().Call("alert", msg)
 }
+
+func ShowConfirm(q string) bool { //LazataknesSoftware
+	re := js.Global().Call("confirm", q).Bool()
+	return re
+}
+
+func ShowPrompt(query string) string { //LazataknesSoftware
+	res := js.Global().Call("prompt", query).String()
+	return res
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ewaldhorn/dommie
-
-go 1.23.5
